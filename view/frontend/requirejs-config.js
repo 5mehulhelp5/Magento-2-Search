6b7ca02a1627aff7bfcce05a473e2a2b055bd231
Jagadeesh Shanmugam <jagadeesh@oceaniasolution.com>
2025-04-22 13:44:37 +0530

    feat(autosuggest): Updated handle bar version

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,7 +2,7 @@ var config = {
 
     paths: {
 
-        "handlebars": "https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.amd.min",
+        "handlebars": "https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.8/handlebars.amd.min",
         "UnbxdSearch": 'https://libraries.unbxdapi.com/search-sdk/v2.1.7/vanillaSearch.min'
     }
 };
